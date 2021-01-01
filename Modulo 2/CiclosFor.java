1222b77eb15b7ebbab6a5c7d13fce7e2e97ee94e
Mauricio Rodríguez <maurodripez@gmail.com>
2020-12-31 22:32:45 -0600

    Todo sobre sumatoria

--- Modulo 2/CiclosFor.java
+++ Modulo 2/CiclosFor.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 public class CiclosFor {
     public static void main(String[] args) {
         CiclosFor ejercicios = new CiclosFor();
-        ejercicios.Factorial();
-        ejercicios.Divisores();
-        ejercicios.NumerosPerfectos();
+        // ejercicios.Factorial();
+        // ejercicios.Divisores();
+        // ejercicios.NumerosPerfectos();
+        ejercicios.Sumatoria();
     }
 
     public void Factorial() {
@@ -38,5 +41,40 @@ public class CiclosFor {
         } else {
             System.out.println(numero + " no es un numero perfecto");
         }
+    }
+
+    public void Sumatoria() {
+        int sumatoria = 0;
+        float resultado = 1;
+        int suma = 0;
+        for (int i = 0; i <= 5; i++) {
+            sumatoria = sumatoria + i;
+
+        }
+        System.out.println("la raiz cuadrada de " + sumatoria + " es = " + Math.sqrt(sumatoria));
+        Random random = new Random();
+        int numeroRandom = random.nextInt(sumatoria + 1);
+        System.out.println("el numero aleatorio entre 1 y " + sumatoria + " es " + numeroRandom);
+        for (int i = 1; i <= sumatoria; i++) {
+
+            if (sumatoria % i == 0) {
+                System.out.println(i + " es divisor de " + sumatoria);
+            }
+        }
+        for (int i = 1; i <= sumatoria; i++) {
+            resultado = resultado * i;
+        }
+        System.out.println("el factorial de " + sumatoria + " es = " + resultado);
+        for (int i = 1; i < sumatoria; i++) {
+            if (sumatoria % i == 0) {
+                suma = suma + i;
+            }
+        }
+        if (suma == sumatoria) {
+            System.out.println(sumatoria + " es un numero perfecto");
+        } else {
+            System.out.println(sumatoria + " no es un numero perfecto");
+        }
+
     }
 }
