5d09bb26bc2277767d901caa5d5927e4e4d43eb3
Mauricio Rodríguez <maurodripez@gmail.com>
2021-01-11 23:26:02 -0600

    proyecto 2

---
+++ Modulo 3/Proyecto2/proyecto2/src/Casa.java
@@ -0,0 +1,37 @@
+public class Casa {
+    private String ciudad = "CDMX";
+    private String barrio = "Tlalpan";
+    private String color = "Amarilla";
+    private int pisos = 1;
+    private int habitaciones = 3;
+    private int banos = 1;
+    private int cocinas = 1;
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getBarrio() {
+        return barrio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPisos() {
+        return pisos;
+    }
+
+    public int getHabitaciones() {
+        return habitaciones;
+    }
+
+    public int getBanos() {
+        return banos;
+    }
+
+    public int getCocinas() {
+        return cocinas;
+    }
+}
---
+++ Modulo 3/Proyecto2/proyecto2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] arg){
+        Casa casa=new Casa();
+        System.out.println(casa.getCiudad());
+        System.out.println(casa.getBarrio());
+        System.out.println(casa.getColor());
+        System.out.println(casa.getPisos());
+        System.out.println(casa.getHabitaciones());
+        System.out.println(casa.getBanos());
+        System.out.println(casa.getCocinas());
+    }
+}
